d97460fedb2d07c3b2753587c302d9e6883f7be5
Ratul3640 <nahanurislam71@gmail.com>
2025-05-06 23:18:29 +0600

    swiper pagination added

--- src/Pages/Shared/Footer/Navbar/Navbar.jsx
+++ src/Pages/Shared/Footer/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     </>
     return (
         <>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar bg-base-350 fixed z-10 bg-opacity-30 text-white shadow-sm">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
